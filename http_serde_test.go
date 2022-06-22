2de9df16dfad51a21d874fe68d568e75f1dc8cd0
Manuel Cordon <manuel.cordon@yalochat.com>
2022-06-22 10:53:14 -0600

    test: Implements integration test using Gin router

--- http_serde_test.go
+++ http_serde_test.go
@@ -2,14 +2,18 @@ package http_serde
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 
 	"github.com/yalochat/http-serde/internal/mocks"
@@ -31,9 +35,11 @@ func TestNew(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.it, func(t *testing.T) {
-			tt.assert(t, New())
-		})
+		t.Run(
+			tt.it, func(t *testing.T) {
+				tt.assert(t, New())
+			},
+		)
 	}
 }
 
@@ -91,13 +97,17 @@ func TestSerialize(t *testing.T) {
 			assert: func(t *testing.T, b []byte, err error) {
 				require.NoError(t, err)
 				require.NotNil(t, b)
-				require.Equal(t, strings.Join([]string{
-					"GET /test HTTP/1.1",
-					"Host: test.test",
-					"Content-Length: 0",
-					"",
-					"",
-				}, "\r\n"), string(b))
+				require.Equal(
+					t, strings.Join(
+						[]string{
+							"GET /test HTTP/1.1",
+							"Host: test.test",
+							"Content-Length: 0",
+							"",
+							"",
+						}, "\r\n",
+					), string(b),
+				)
 			},
 		},
 		{
@@ -111,22 +121,28 @@ func TestSerialize(t *testing.T) {
 			assert: func(t *testing.T, b []byte, err error) {
 				require.NoError(t, err)
 				require.NotNil(t, b)
-				require.Equal(t, strings.Join([]string{
-					"POST / HTTP/1.1",
-					"Host: test.test",
-					"Content-Length: 4",
-					"",
-					"test",
-				}, "\r\n"), string(b))
+				require.Equal(
+					t, strings.Join(
+						[]string{
+							"POST / HTTP/1.1",
+							"Host: test.test",
+							"Content-Length: 4",
+							"",
+							"test",
+						}, "\r\n",
+					), string(b),
+				)
 			},
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.it, func(t *testing.T) {
-			req := tt.setup(t)
-			got, err := New().Serialize(req)
-			tt.assert(t, got, err)
-		})
+		t.Run(
+			tt.it, func(t *testing.T) {
+				req := tt.setup(t)
+				got, err := New().Serialize(req)
+				tt.assert(t, got, err)
+			},
+		)
 	}
 }
 
@@ -209,10 +225,87 @@ func TestDeserialize(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.it, func(t *testing.T) {
-			s := tt.setup(t)
-			got, err := New().Deserialize(s)
-			tt.assert(t, got, err)
-		})
+		t.Run(
+			tt.it, func(t *testing.T) {
+				s := tt.setup(t)
+				got, err := New().Deserialize(s)
+				tt.assert(t, got, err)
+			},
+		)
+	}
+}
+
+func TestGinIntegration(t *testing.T) {
+	tests := []struct {
+		it     string
+		setup  func(t *testing.T, ctx context.Context, out chan<- []byte) (http.Handler, *http.Request)
+		assert func(t *testing.T, r *http.Request, err error)
+	}{
+		{
+			it: "serializes incoming request from gin handler",
+			setup: func(t *testing.T, ctx context.Context, out chan<- []byte) (http.Handler, *http.Request) {
+				router := gin.Default()
+				router.GET(
+					"/serialize", func(ctx *gin.Context) {
+						got, err := New().Serialize(ctx.Request)
+						require.NoError(t, err)
+						require.NotNil(t, got)
+						out <- got
+					},
+				)
+				request, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/serialize", nil)
+				return router, request
+			},
+			assert: func(t *testing.T, r *http.Request, err error) {
+
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			tt.it, func(t *testing.T) {
+				serDe := New()
+				ctx, cancelFunc := context.WithTimeout(context.Background(), time.Hour)
+				defer cancelFunc()
+				out := make(chan []byte, 1)
+				handler, request := tt.setup(t, ctx, out)
+				s := http.Server{
+					Addr:    ":8080",
+					Handler: handler,
+				}
+				go func() {
+					err := s.ListenAndServe()
+					require.True(t, err != nil && errors.Is(err, http.ErrServerClosed), "got error", err)
+				}()
+
+				response, err := http.DefaultClient.Do(request)
+				require.NoError(t, err)
+				require.Equal(t, http.StatusOK, response.StatusCode)
+
+				deserialized, err := serDe.Deserialize(<-out)
+				require.NoError(t, err)
+
+				// Request URL contains the path within the instance.
+				deserialized.URL, err = url.Parse("http://localhost" + s.Addr + deserialized.RequestURI)
+
+				// Request URI must be empty in order to be executed, else we get
+				// **http: Request.RequestURI can't be set in client requests** error
+				deserialized.RequestURI = ""
+
+				require.NoError(t, err)
+				response, err = http.DefaultClient.Do(deserialized)
+				require.NoError(t, err)
+				require.Equal(t, http.StatusOK, response.StatusCode)
+				require.NoError(t, s.Shutdown(ctx))
+				deserialized2, err := serDe.Deserialize(<-out)
+				require.NoError(t, err)
+
+				// Since both URLs are different memory addresses, direct comparison doesn't work.
+				b1, _ := serDe.Serialize(deserialized)
+				b2, _ := serDe.Serialize(deserialized2)
+				require.Equal(t, b1, b2)
+			},
+		)
 	}
 }
